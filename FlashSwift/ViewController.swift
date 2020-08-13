aabb3477a96a5d09fc522419b19db1e9328b3793
Patricia <patysiq@yahoo.com.br>
2020-08-13 11:04:40 -0300

    Initial Commit

---
+++ FlashSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashSwift
+//
+//  Created by PATRICIA S SIQUEIRA on 13/08/20.
+//  Copyright © 2020 PATRICIA S SIQUEIRA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FlashSwiftTests/FlashSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  FlashSwiftTests.swift
+//  FlashSwiftTests
+//
+//  Created by PATRICIA S SIQUEIRA on 13/08/20.
+//  Copyright © 2020 PATRICIA S SIQUEIRA. All rights reserved.
+//
+
+import XCTest
+@testable import FlashSwift
+
+class FlashSwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
